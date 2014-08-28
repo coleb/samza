d81278a2c1739aaeb25102764fa641732a2eef7d
Jon Bringhurst <jon@bringhurst.org>
2014-08-27 22:21:58 -0400

    Formatting.

--- samza-mesos/src/main/scala/org/apache/samza/job/mesos/constraints/NumericalConstraint.scala
+++ samza-mesos/src/main/scala/org/apache/samza/job/mesos/constraints/NumericalConstraint.scala
@@ -5,12 +5,11 @@ import org.apache.mesos.Protos.{Attribute, TaskInfo, Offer}
 import scala.collection.JavaConversions._
 import scala.concurrent.Future
 
-
 class NumericalConstraint(offers: java.util.Collection[Offer], tasks: java.util.Collection[TaskInfo]) extends SchedulingConstraint {
   val name: Option[String]
   val value: Option[Any]
 
-  /** Determine if an offer satisfy's the constraint. */
+  /** Determine if an offer satisfies the constraint. */
   def offerIsSatisfied(offer: Offer): Boolean = {
     offer.getAttributesList.forall((attr: Attribute) =>
       (name, value, Option(attr.getScalar)) match {
